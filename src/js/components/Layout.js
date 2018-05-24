b096c2396074b2710feb2197507ae2426f102f51
Dani Vijay <danimvijay@gmail.com>
2018-05-24 10:31:11 +0530

    state

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,21 @@ import Header from './Header'
 import Footer from './Footer'
 
 export default class Layout extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      name: "dani"
+    }
+  }
   render() {
+    setTimeout(() => {
+      this.setState({
+        name: "dani m v"
+      })
+    }, 1000)
     return (
       <div>
+        { this.state.name }
         <Header />
         <Footer />
       </div>
